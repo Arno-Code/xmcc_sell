54b983de2b9a3a76ba1900ecf92781ecaea66a8c
xmcc <1355005843@qq.com>
2019-04-22 14:22:45 +0800

    第三次上传-19/04/22:微信授权

---
+++ src/main/java/com/xmcc/config/WechatProperties.java
@@ -0,0 +1,17 @@
+package com.xmcc.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "wechat")
+public class WechatProperties {
+
+    private String appId;
+
+    private String secret;
+
+}
